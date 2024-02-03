6c8a515de2fb17689d4b58969c266cddc47d4abc
Christopher <114704469+Blade-24@users.noreply.github.com>
2024-02-02 22:45:33 -0600

    update Technology section

--- src/pages/Technology.tsx
+++ src/pages/Technology.tsx
@@ -1,17 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SpaceBackgroundTechnologyPhone from "../assets/technology/background-technology-mobile.jpg";
+import { TitleSection } from "../components/TitleSection";
+import FirstStep from "../assets/technology/image-launch-vehicle-landscape.jpg";
+import SecondStep from "../assets/technology/image-spaceport-landscape.jpg";
+import ThirdStep from "../assets/technology/image-space-capsule-landscape.jpg";
+
+const API_ROUTE = "/data.json";
+
+interface Technology {
+  name: string;
+  description: string;
+}
 
 function Technology() {
+  const [technology, setTechnology] = useState();
+  const [selectedTechnology, setSelectedTechnology] = useState(0);
+
+  useEffect(() => {
+    fetch(API_ROUTE)
+      .then((res) => res.json())
+      .then((data) => {
+        const { technology } = data;
+        setTechnology(technology[selectedTechnology]);
+        console.log(technology[selectedTechnology]);
+      });
+  }, [selectedTechnology]);
+
+  const ChangeImageTech = () => {
+    switch (selectedTechnology) {
+      case 0: {
+        return FirstStep;
+      }
+      case 1: {
+        return SecondStep;
+      }
+      case 2: {
+        return ThirdStep;
+      }
+    }
+  };
+
   return (
     <section
       id="technology"
-      className="Page hidden h-dvh w-dvw absolute top-0 left-0"
+      className="Page hidden h-dvh w-dvw relative top-0 left-0"
     >
       <img
         src={SpaceBackgroundTechnologyPhone}
         alt="Background technology"
         className="w-dvw h-dvh absolute top-0 left-0"
       />
+
+      <article className="absolute bottom-0 left-0 w-full h-[90%] flex justify-center items-center flex-col">
+        <TitleSection numberSection={3} titleSection="space launch 101" />
+
+        <figure className="h-[35%] my-3">
+          <img
+            src={ChangeImageTech()}
+            alt="Image"
+            className="h-full object-cover"
+          />
+        </figure>
+
+        <nav className="w-full flex justify-center gap-4">
+          {[0, 1, 2].map((item) => (
+            <div
+              key={item}
+              className="text-white h-14 w-14 border-white border flex justify-center items-center rounded-full hover:cursor-pointer"
+              onClick={() => setSelectedTechnology(item)}
+            >
+              <h3 className="text-2xl">{item + 1}</h3>
+            </div>
+          ))}
+        </nav>
+
+        <section className="w-[90%] mx-auto text-center my-5 text-white">
+          <p className="uppercase text-xl">The terminology...</p>
+          <h3 className="text-4xl uppercase font-Bellefair my-2">
+            {technology?.name || ""}
+          </h3>
+          <p className="text-xl text-pretty">{technology?.description || ""}</p>
+        </section>
+
+        <section className="w-full"></section>
+      </article>
     </section>
   );
 }
